cb6b0c420e2241eddb1a74831a111533939319d5
Tim Möhlmann <muhlemmer@gmail.com>
2020-10-13 18:36:26 +0300

    Update integration/sqlboiler

--- integration/sqlboiler/go.mod
+++ integration/sqlboiler/go.mod
@@ -3,6 +3,6 @@ module github.com/moapis/multidb/integration/sqlboiler
 go 1.15
 
 require (
-	github.com/moapis/multidb v0.1.4-0.20201008130032-6bc4c5a7c199
+	github.com/moapis/multidb v0.1.4-0.20201013152111-389880bfb0c7
 	github.com/volatiletech/sqlboiler/v4 v4.2.0
 )
--- integration/sqlboiler/sqlboiler.go
+++ integration/sqlboiler/sqlboiler.go
@@ -5,15 +5,10 @@ import (
 	"github.com/volatiletech/sqlboiler/v4/boil"
 )
 
-func _() boil.Executor        { return multidb.MultiNode{} }
-func _() boil.ContextExecutor { return multidb.MultiNode{} }
+func _() boil.Executor        { return &multidb.MultiNode{} }
+func _() boil.ContextExecutor { return &multidb.MultiNode{} }
 
 func _() boil.Executor          { return &multidb.MultiTx{} }
 func _() boil.ContextExecutor   { return &multidb.MultiTx{} }
 func _() boil.Transactor        { return &multidb.MultiTx{} }
 func _() boil.ContextTransactor { return &multidb.MultiTx{} }
-
-func _() boil.Executor        { return &multidb.Node{} }
-func _() boil.ContextExecutor { return &multidb.Node{} }
-func _() boil.Beginner        { return &multidb.Node{} }
-func _() boil.ContextBeginner { return &multidb.Node{} }
